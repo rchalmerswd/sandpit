33740bd04ed096cadee1b7939f6c3b1c5ae71717
Ryan Chalmers <rchalmers2801@gmail.com>
2022-10-27 15:58:45 +0100

    0

--- templates/templateinherit.py
+++ templates/templateinherit.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/template.html")
+@app.route("/inherits.html")
 def inherits():
-    return render_template("template.html")
+    return render_template("base.html")
 
-@app.route("/extensions1.html")
+@app.route("/extensions1")
 def extensions_1():
     return render_template("template.html")
 
