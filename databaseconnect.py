23194aecd0f6cff8bf155a3e1670e718a1af8474
Ryan Chalmers <rchalmers2801@gmail.com>
2022-10-27 16:27:59 +0100

    Database Set Up

---
+++ databaseconnect.py
@@ -0,0 +1,15 @@
+import sqlite3
+conn = sqlite3.connect('test_database')
+c = conn.cursor()
+
+c.execute('''
+CREATE TABLE IF NOT EXISTS products
+([product_id] INTERGER PRIMARY KEY, [product_name] TEXT)
+''')
+
+c.execute('''
+CREATE TABLE IF NOT EXISTS prices
+([product_id] INTEGER PRIMARY KEY, [product_name] TEXT)
+''')
+
+conn.commit()
